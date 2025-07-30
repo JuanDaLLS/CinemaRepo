ec101b8718ec25c0ae3e558037d7124f8dded79d
JuanDaLLS <juanulin90@gmail.com>
2025-07-29 19:07:26 -0600

    Updte test

--- cineFacil.py
+++ cineFacil.py
@@ -1,7 +1,7 @@
 availableMovies = ["matrix", "prisioners", "el camino", "hereditary", "midsommar"]
 n= 1
 ticket = 35
-reserves = []
+reserves = {}
 
 name = input("Ingrese su nombre: " )
 print(f"Estas son las peliculas disponibles:" )
@@ -14,5 +14,4 @@ if movie in availableMovies:
     howMany = int(input(f"Vas a ver: {movie}" f"\nPrecio por entrada: {ticket}" f"\nCuantos tickets quieres comprar? "))
     price = ticket * howMany
     print(f"----- Resumen de compra ------"f"\nTu nombre: {name}" f"\nVas a ver: {movie}" "\n----Pago total ----", f"\nQ{price}")
-    
 else: print("Titulo incorrecto, prueba de nuevo amigo")
